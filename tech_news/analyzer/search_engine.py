7e08a7cac8442b06815ac955f6b00d0f0f3a4621
Leandro Lopes <leandro_carlos@uol.com.br>
2022-02-14 18:28:20 -0300

    req 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 # Requisito 6
@@ -13,7 +14,15 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Data inválida")
+    date_news = search_news({"timestamp": {"$regex": date}})
+    list_news = []
+    for news in date_news:
+        list_news.append((news["title"], news["url"]))
+    return list_news
 
 
 # Requisito 8
